ed4c45845fb6748741360be9d33596206e5b00ae
Yuri Lee <yujatea97@snu.ac.kr>
2026-03-26 20:19:08 +0900

    fix: biome lint (import sort, trailing comma)

--- src/api/notification.ts
+++ src/api/notification.ts
@@ -3,7 +3,7 @@ import apiClient from './index';
 export const registerDevice = (
   deviceId: string,
   fcmToken: string,
-  browserType: string,
+  browserType: string
 ) =>
   apiClient.post('/user/device', {
     token: fcmToken,
